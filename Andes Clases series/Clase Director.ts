2bcb3aa6881cd95edc14c37f81260046f2518823
AnaLeguia <128002888+AnaLeguia@users.noreply.github.com>
2023-05-25 11:01:26 -0500

    Andes clases entrega #2

---
+++ Andes Clases series/Clase Director.ts
@@ -0,0 +1,14 @@
+class Director {
+    private nombre: string;
+    private fotografia: string;
+    private descripcion: string;
+    private seriesDirigidas: Serie[];
+  
+    constructor(nombre: string, fotografia: string, descripcion: string) {
+      this.nombre = nombre;
+      this.fotografia = fotografia;
+      this.descripcion = descripcion;
+      this.seriesDirigidas = [];
+    }
+}  
+
---
+++ Andes Clases series/Clase Serie.ts
@@ -0,0 +1,14 @@
+class Serie {
+  categoria: string;
+  imagenes: string[];
+  nombre: string;
+  episodios: number;
+
+  constructor(categoria: string, imagenes: string[], nombre: string, episodios: number) {
+    this.categoria = categoria;
+    this.imagenes = imagenes;
+    this.nombre = nombre;
+    this.episodios = episodios;
+  }
+}
+
